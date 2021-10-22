271a63297fbad89f8897031d5ce43b3c560cbb2c
Necryl <necrylthewayfarer@gmail.com>
2021-10-22 11:09:49 +0530

    setting up display loop

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
 import pygame
+
+# Colors
+paint = {
+    "black": (0, 0, 0),
+    "green": (0, 255, 0),
+    "red": (255, 0, 0)
+}
+
+# Window
+screen_size = (900, 700)
+screen = pygame.display.set_mode(screen_size)
+pygame.display.set_caption("Snake eats apples??")
+
+def play():
+    carry_on = True
+
+    clock = pygame.time.Clock()
+
+    while carry_on:
+
+        carry_on = handle_events()
+
+        screen.fill(paint["black"])
+
+        pygame.display.update()
+
+    pygame.quit()
+
+
+def handle_events():
+
+    result = True
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            result = False
+    
+    return result
+
+play()
